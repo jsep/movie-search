efed640ee6609ab0188d879cbbcb5d3871fd895e
Juan Sepulveda <me@juan.do>
2023-01-09 14:13:24 +0000

    Add first e2e test

--- src/e2e/home.spec.ts
+++ src/e2e/home.spec.ts
@@ -1,10 +1,9 @@
-import { test, expect } from '@playwright/test'
+import { test, expect } from '@playwright/test';
 
-test('should navigate to the about page', async ({ page }) => {
+test('should navigate to the home page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   // TODO configure baseURL in playwright.config.ts using env variable
-  await page.goto('http://localhost:3000/')
-  await expect(page.locator('h1')).toContainText('Movie Search')
-  await expect(page.locator("#search")).toHaveCount(1)
-  await expect(page.locator("#search-btn")).toContainText("Search")
-})
+  await page.goto('http://localhost:3000/');
+  await expect(page.locator('h1')).toContainText('Movie Search');
+  await expect(page.locator('#search')).toHaveCount(1);
+});
