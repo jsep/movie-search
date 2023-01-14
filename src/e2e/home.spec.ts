6a4d97af101b647ee67a28eab7e8fa13faa4638b
nachlielEasy <nachliel@easyappshq.com>
2023-01-14 18:52:45 +0200

    Test automated testing with meant to fail test

--- src/e2e/home.spec.ts
+++ src/e2e/home.spec.ts
@@ -4,6 +4,6 @@ test('should navigate to the home page', async ({ page }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   // TODO configure baseURL in playwright.config.ts using env variable
   await page.goto('http://localhost:3000/');
-  await expect(page.locator('h1')).toContainText('Movie Search');
+  await expect(page.locator('h1')).toContainText('Movie and Search');
   await expect(page.locator('#search')).toHaveCount(1);
 });
