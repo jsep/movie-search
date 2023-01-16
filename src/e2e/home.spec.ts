c45f1fc681007fb41ea5dcad602ea3aa37383f9a
nachlielEasy <nachliel@easyappshq.com>
2023-01-16 16:49:08 +0200

    Config e2e test to run after vercel deployment

--- src/e2e/home.spec.ts
+++ src/e2e/home.spec.ts
@@ -1,9 +1,10 @@
 import { test, expect } from '@playwright/test';
 
-test('should navigate to the home page', async ({ page }) => {
+test('should navigate to the home page', async ({ page,baseURL }) => {
   // Start from the index page (the baseURL is set via the webServer in the playwright.config.ts)
   // TODO configure baseURL in playwright.config.ts using env variable
-  await page.goto('http://localhost:3000/');
-  await expect(page.locator('h1')).toContainText('Movie and Search');
+  console.log("baseURL: ",baseURL)
+  await page.goto(baseURL);
+  await expect(page.locator('h1')).toContainText('Movie Search');
   await expect(page.locator('#search')).toHaveCount(1);
 });
