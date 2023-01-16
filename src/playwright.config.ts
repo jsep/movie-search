32bf31e5de30a0ab7b08d6db4b57be50f2f75809
nachlielEasy <101057295+nachlielEasy@users.noreply.github.com>
2023-01-16 16:55:20 +0200

    Automate test and deploy (#2)
    * Test automated testing with meant to fail test
    
    * Config e2e test to run after vercel deployment

--- src/playwright.config.ts
+++ src/playwright.config.ts
@@ -1,10 +1,9 @@
 import { PlaywrightTestConfig } from '@playwright/test';
 
 const config: PlaywrightTestConfig = {
-  webServer: {
-    command: 'pnpm ci:start',
-    url: 'http://localhost:3000',
-  },
+  use:{
+    baseURL:'http://localhost:3000',
+  }
 };
 
 export default config;
